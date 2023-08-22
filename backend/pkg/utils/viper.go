58d4b6af795c574d8b214727eddabc42ff6db80c
wulinlin <18574945291@163.com>
2023-08-22 11:50:56 +0800

    project config file and parse

--- backend/pkg/utils/viper.go
+++ backend/pkg/utils/viper.go
@@ -1,6 +1,15 @@
 package utils
 
-import "flag"
+import (
+	"backend/pkg/config"
+	"backend/pkg/utils/strs"
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/spf13/viper"
+)
 
 //
 // @Description
@@ -15,4 +24,34 @@ func InitViper() {
 	flag.StringVar(&filePath, "c", "", "input config file .")
 	flag.Parse()
 
+	if strs.IsBlank(filePath) {
+		log.Printf("命令行读取参数, 配置文件路径为: %s\n", filePath)
+	} else {
+		log.Println("命令行参数为空, 默认加载: ./settings_dev.yml")
+		filePath = "./settings_dev.yml"
+	}
+	// 目前读取固定固定路径的配置文件
+	v := viper.New()
+	v.SetConfigFile(filePath)
+	v.AutomaticEnv()                                   // 允许使用环境变量
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_")) // SERVER_APPMODE => SERVER.APPMODE
+	// 读取配置文件
+	if err := v.ReadInConfig(); err != nil {
+		log.Panic("配置文件读取失败: ", err)
+	}
+
+	// 加载配置文件内容到结构体对象
+	if err := v.Unmarshal(&config.Cfg); err != nil {
+		log.Panic("配置文件内容加载失败: ", err)
+	}
+	fmt.Println(config.Cfg.Email)
+	// TODO: 配置文件热重载, 使用场景是什么?
+	// v.WatchConfig()
+	// v.OnConfigChange(func(e fsnotify.Event) {
+	// 	log.Println("检测到配置文件内容修改")
+	// 	if err := v.Unmarshal(&config.Cfg); err != nil {
+	// 		log.Panic("配置文件内容加载失败: ", err)
+	// 	}
+	// })
+	log.Println("配置文件内容加载成功")
 }
